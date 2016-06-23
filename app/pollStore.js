182fe858eba8f20031e100452c504920d0b0ba44
Adrien Joly <adrien@whyd.com>
2016-06-23 14:20:49 +0200

    first working version of 1poll (create + fetch) using firebase instead of parse

--- app/pollStore.js
+++ app/pollStore.js
@@ -1,48 +1,57 @@
+var firebase = require("firebase");
+//require("firebase/app");
+//require("firebase/auth");
+//require("firebase/database");
 module.exports = (function() {
 
   var inProd = window.location.href.indexOf('http://localhost:') == -1;
 
-  if (inProd) {
-    Parse.initialize('he01O47RD3Xf5HQcOf5lKJzoQ4yZLeQvWYbOxUcN', 'afCgncModOMnQSeWxTcOMxaaETl91Kw74G6XlyHI'); // heroku instance
-  }
-  else {
-    Parse.initialize('EW02RQhhvjE3B58YDgbo87dqRWYCiJeZyusD8ll7', 'UTu46JdJfO7VrD30GkmMJHAL5TgqQysLgh24JZlf');
-  }
-  
-  Parse.serverURL = inProd
-    ? 'https://parse-1poll.herokuapp.com/parse'
-    : 'http://localhost:1337/parse';
-  
-  var Poll = Parse.Object.extend('Poll');
+  // Initialize Firebase
+  firebase.initializeApp({
+    apiKey: "AIzaSyCBOU-8RLKzGlMnbuOMREAut34VcW4M8Zw",
+    authDomain: "poll-edd8e.firebaseapp.com",
+    databaseURL: "https://poll-edd8e.firebaseio.com",
+    storageBucket: "poll-edd8e.appspot.com",
+  });
+
+  var polls = firebase.database().ref().child('polls');
 
   function render(obj) {
-    return obj && {
-      objectId: obj.id,
-      title: obj.get('title'),
-      subtitle: obj.get('subtitle'),
-      options: obj.get('options').map((opt) => {
+    console.log('render obj:', obj);
+    if (!obj) return;
+    var data = obj.val();
+    console.log('render obj data:', data);
+    return {
+      objectId: obj.key,
+      title: data.title,
+      subtitle: data.subtitle,
+      options: data.options.map((opt) => {
         return { name: opt };
       })
     };
   }
 
   function fetch(id, cb) {
-    var query = new Parse.Query(Poll);
-    query.get(id, {
-      success: (obj) => { cb(null, render(obj)); },
-      error: (obj, err) => { cb(err, render(obj)); }
-    });
+    console.log('fetch', id);
+    firebase.database().ref('polls/' + id).once('value').then((poll) => {
+      console.log('fetch', id, '->', arguments);
+      cb(null, render(poll));
+    }, cb);
   }
 
   function save(pollData, cb) {
     console.log('storing poll:', pollData);
-    (new Poll()).save(pollData, {
-      success: (obj) => { cb(null, render(obj)); },
-      error: (obj, err) => { cb(err, render(obj)); }
+    var poll = polls.push();
+    poll.set(pollData, function(err){
+      console.log('save -> set', arguments);
+      fetch(poll.key, cb);
+      //cb(err, !err && render(poll));
     });
   }
   
   function vote(pollId, voteObj) {
+    return;
+    /*
     console.log('voting for poll options:', pollId, voteObj);
     var query = new Parse.Query(Poll);
     query.get(pollId).then(function(poll) {
@@ -58,6 +67,7 @@ module.exports = (function() {
       alert('There was a problem while saving your vote, sorry...');
       throw "Got an error " + error.code + " : " + error.message;
     });
+    */
   }
 
   return {
